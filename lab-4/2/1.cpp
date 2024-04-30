9c4b315a0840922a0e86b7b63a3da6c5c37b059c
nextples <gankeev@gmail.com>
2024-05-01 04:06:24 +0700

    add 4.2

---
+++ lab-4/2/1.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    printf("pid: %d\n", getpid());
+    sleep(1);
+
+    execv(argv[0], argv);
+    printf("Hello world");
+}
