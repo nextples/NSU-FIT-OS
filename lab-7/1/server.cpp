af9200a0a58562ce3aee9a9c6e4df96b98519bda
nextples <gankeev@gmail.com>
2024-05-23 00:42:36 +0700

    add 7.1

---
+++ lab-7/1/server.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+    ssize_t received_len;
+
+    // Создание сокета
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Настройка адреса сервера
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    // Привязка сокета к адресу
+    if (bind(sockfd, (const struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("UDP server is up and listening on port %d\n", PORT);
+
+    // Основной цикл сервера
+    while (1) {
+        received_len = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, 0,
+                                (struct sockaddr *)&client_addr, &addr_len);
+        buffer[received_len] = '\0';
+        printf("Received message from %s:%d: %s\n",
+               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buffer);
+
+        // Отправка сообщения обратно клиенту
+        sendto(sockfd, (const char *)buffer, received_len, 0,
+               (const struct sockaddr *)&client_addr, addr_len);
+    }
+
+    close(sockfd);
+    return 0;
+}
