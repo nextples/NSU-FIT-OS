76668c5153259d939784e216f1c51b33c466aab8
nextples <gankeev@gmail.com>
2024-09-27 15:12:22 +0700

    add task1.3

---
+++ sem2/lab-1/task13-1.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+struct test_t {
+    int num;
+    char* str;
+};
+
+void* thread_routine(void* arg) {
+    struct test_t* test_arg = (struct test_t*) arg;
+
+    printf("thread_routine [%d %d %d %ld]: Data: [num=%d; str=%s]\n",
+            getpid(), getppid(), gettid(), pthread_self(), test_arg->num, test_arg->str);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+
+    struct test_t test_arg;
+    test_arg.num = 111;
+    test_arg.str = "Hello";
+
+    pthread_create(&thread, NULL, thread_routine, (void*) &test_arg);
+
+    pthread_join(thread, NULL);
+
+    return 0;
+}
---
+++ sem2/lab-1/task13-2.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+struct test_t {
+    int num;
+    char* str;
+};
+
+void* thread_routine(void* arg) {
+    struct test_t* test_arg = (struct test_t*) arg;
+
+    printf("Thread routine\n");
+    printf("thread_routine [%d %d %d %ld]: Data: [num=%d; str=%s]\n",
+                getpid(), getppid(), gettid(), pthread_self(), test_arg->num, test_arg->str);
+    return NULL;
+}
+
+void* main_routine(void* arg) {
+    pthread_t thread;
+    pthread_attr_t attr;
+
+    struct test_t* test_arg = (struct test_t*) malloc(sizeof(struct test_t));
+    test_arg->num = 111;
+    test_arg->str = "Hello";
+
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+    pthread_create(&thread, &attr, thread_routine, (void *) test_arg);
+
+    sleep(5);
+    free(test_arg);
+
+    pthread_attr_destroy(&attr);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+
+    pthread_create(&thread, NULL, main_routine, NULL);
+    pthread_join(thread, NULL);
+
+    sleep(5);
+    return 0;
+}
